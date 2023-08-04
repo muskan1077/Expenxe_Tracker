9ef4bd93ebfbceef7d48a26b1da10f89d805d715
muskan1077 <muskan.1077gupta@gmail.com>
2023-08-04 11:30:30 +0530

    prefinal_commit

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -3,18 +3,18 @@ import React from 'react';
 import ExpenseForm from './ExpenseForm';
 import './NewExpense.css';
 
-const NewExpense = () => {
-  const saveExpenseDataHandler = (enteredExpenseData) =>{
-  const expenseData = {
-    ...enteredExpenseData,
-    id: Math.random().toString()
+const NewExpense = (props) => {
+  const saveExpenseDataHandler = (enteredExpenseData) => {
+    const expenseData = {
+      ...enteredExpenseData,
+      id: Math.random().toString()
+    };
+    props.onAddExpense(expenseData);
   };
-  console.log(expenseData);
-  props.onAddExpense(expenseData);
-  };
+
   return (
     <div className='new-expense'>
-      <ExpenseForm onSaveExpenseData = {saveExpenseDataHandler} />
+      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
     </div>
   );
 };
